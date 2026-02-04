afcd7c72e947440e57c723cf32067458c3665fce
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2026-02-04 20:14:38 +0900

    fix: iam role
    Signed-off-by: Yoshio Sugiyama <nenegi.01mo@gmail.com>

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -21,10 +21,17 @@ resource "google_project_iam_member" "dify_sa_metric_writer" {
   member  = "serviceAccount:${google_service_account.dify_sa.email}"
 }
 
-resource "google_project_iam_member" "dify_sa_storage_admin" {
+resource "google_project_iam_member" "dify_sa_storage_viewer" {
   project = var.project_id
-  role    = "roles/storage.objectAdmin"
+  role    = "roles/storage.bucketViewer"
   member  = "serviceAccount:${google_service_account.dify_sa.email}"
+}
+
+# Grant bucket-level permissions
+resource "google_storage_bucket_iam_member" "dify_storage_object_admin" {
+  bucket = var.storage_bucket_name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${google_service_account.dify_sa.email}"
 }
 
 # Service Account Key for GCS access (optional, only if needed outside GCE)
